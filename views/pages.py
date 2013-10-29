849087f22d25b4a74a214b958d0c40a3934e8b5a
Craig M. Stimmel <cstimmel@uw.edu>
2013-10-29 13:59:55 -0700

    IS_DESKTOP param is now handled by a is_desktop context processor.

--- views/pages.py
+++ views/pages.py
@@ -5,19 +5,13 @@ from django.shortcuts import render_to_response
 # Create your views here.
                         
 def home(request):
-    params = { 
-        'IS_DESKTOP' : not request.MOBILE, 
-    }
+    params = {}
     return render_to_response('pages/home.html', params, context_instance=RequestContext(request))
 
 def basic(request):
-    params = { 
-        'IS_DESKTOP' : not request.MOBILE, 
-    }
+    params = {}
     return render_to_response('pages/basic.html', params, context_instance=RequestContext(request))
 
 def hybrid(request):
-    params = { 
-        'IS_DESKTOP' : not request.MOBILE,
-    }
+    params = {}
     return render_to_response('pages/hybrid.html', params, context_instance=RequestContext(request))
