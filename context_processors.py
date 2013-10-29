a3b9d3f0d083d54689139ab38ca0795f62f62cac
Charlon Palacay <charlon@gmail.com>
2013-10-28 21:41:42 -0700

    Fixes context processor and moves glyph fonts into css directory.

--- context_processors.py
+++ context_processors.py
@@ -6,10 +6,10 @@ TEMPLATE_CONTEXT_PROCESSORS setting.
 """
 from django.conf import settings
 
-def is_desktop(request)
+def is_desktop(request):
     """ See if the browser is desktop
     """
     if request.MOBILE == 0:
-        return {'is_desktop': True}
-    else
-        return {'is_desktop': False}
+        return {'is_desktop' : True}
+    else:
+        return {'is_desktop' : False}
